298e889c3398438e917068c8700dc19652372c20
Bhaskarkr64 <bhaskarkr65@gmail.com>
2025-07-10 07:11:44 +0530

    few changes

--- app/src/main/java/eu/tutorials/myshoppinglistapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/myshoppinglistapp/MainActivity.kt
@@ -1,16 +1,25 @@
 package eu.tutorials.myshoppinglistapp
 
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import eu.tutorials.myshoppinglistapp.ui.theme.MyShoppingListAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +29,33 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyShoppingListAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    var sItems by remember{ mutableStateOf<ShoppingItem>() }
+                    Column(
+                        modifier = Modifier.fillMaxSize(),
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Button(
+                            onClick = {},
+                            modifier = Modifier.align(Alignment.CenterHorizontally) )
+                        {
+                            Text(text = "Add Item")
+                        }
+                        LazyColumn(
+                            modifier = Modifier.fillMaxSize().padding(16.dp)
+                        ) {
+                            items(sItems)
+                        }
+                    }
                 }
             }
+
         }
     }
 }
+data class ShoppingItem(
+    val id: String,
+    var name: String,
+    var quantity: Int,
+    var isEditing: Boolean = false
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    MyShoppingListAppTheme {
-        Greeting("Android")
-    }
-}
+)
