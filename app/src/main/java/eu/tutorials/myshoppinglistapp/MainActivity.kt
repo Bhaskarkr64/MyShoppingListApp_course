f4c932176355667496e84af12d8f4b23e8297aff
Bhaskarkr64 <bhaskarkr65@gmail.com>
2025-07-10 10:56:36 +0530

    correct code done

--- app/src/main/java/eu/tutorials/myshoppinglistapp/MainActivity.kt
+++ app/src/main/java/eu/tutorials/myshoppinglistapp/MainActivity.kt
@@ -10,12 +10,15 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -29,33 +32,38 @@ class MainActivity : ComponentActivity() {
         setContent {
             MyShoppingListAppTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    var sItems by remember{ mutableStateOf<ShoppingItem>() }
+                    var sItems by remember { mutableStateOf(listOf<ShoppingItem>()) }
                     Column(
-                        modifier = Modifier.fillMaxSize(),
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
                         verticalArrangement = Arrangement.Center
                     ) {
                         Button(
                             onClick = {},
-                            modifier = Modifier.align(Alignment.CenterHorizontally) )
-                        {
+                            modifier = Modifier.align(Alignment.CenterHorizontally)
+                        ) {
                             Text(text = "Add Item")
                         }
                         LazyColumn(
-                            modifier = Modifier.fillMaxSize().padding(16.dp)
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
                         ) {
-                            items(sItems)
+                            items(sItems) { item ->
+                                Text(text = "${item.name} (${item.quantity})")
+                            }
                         }
                     }
                 }
             }
-
         }
     }
 }
+
 data class ShoppingItem(
     val id: String,
     var name: String,
     var quantity: Int,
     var isEditing: Boolean = false
-
 )
